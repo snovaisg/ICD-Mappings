73b7d0e465d28d58f8de7f7b535509f278a57e29
Simao Novais <snovaisg.97@gmail.com>
2023-10-14 15:45:17 +0100

    fixed tests

--- tests/test_mapper.py
+++ tests/test_mapper.py
@@ -22,13 +22,12 @@ def test_mapper():
     expected_level_3 = '567'
     expected_cci = False
 
-    assert expected_icd10code == mapper.map(icd9code, 'icd9toicd10')
-    assert expected_ccscode == mapper.map(icd9code, 'icd9toccs')
-    assert expected_chapter == mapper.map(icd9code, 'icd9tochapter')
+    assert expected_icd10code == mapper.map(icd9code, source='icd9', target='icd10')
+    assert expected_ccscode == mapper.map(icd9code, source='icd9', target='ccs')
+    assert expected_cci == mapper.map(icd9code, source='icd9', target='cci')
+    assert expected_chapter == mapper.map(icd9code, source='icd9', target='chapter')
     assert expected_icd9dxvalidity == mapper.validate_diagnostics(icd9code, 'icd9')
     assert expected_icd9procvalidity == mapper.validate_procedures(icd9code, 'icd9')
-    assert expected_level_3 == mapper.map(icd9code, 'icd9tolevel3')
-    assert expected_cci == mapper.map(icd9code, 'icd9tocci')
 
     icd9codes = ['5679', 235, '6010']
     expected_icd10codes = ['K659', None, 'N410']
@@ -39,11 +38,10 @@ def test_mapper():
     expected_level_3 = ['567', None, '601']
     expected_cci = [False, None, False]
 
-    assert expected_icd10codes == mapper.map(icd9codes, 'icd9toicd10')
-    assert expected_ccscodes == mapper.map(icd9codes, 'icd9toccs')
-    assert expected_chapters == mapper.map(icd9codes, 'icd9tochapter')
+    assert expected_icd10codes == mapper.map(icd9codes, source='icd9', target='icd10')
+    assert expected_ccscodes == mapper.map(icd9codes, source='icd9', target='ccs')
+    assert expected_cci == mapper.map(icd9codes, source='icd9', target='cci')
+    assert expected_chapters == mapper.map(icd9codes, source='icd9', target='chapter')
     assert expected_icd9dxvalidity == mapper.validate_diagnostics(icd9codes, 'icd9')
     assert expected_icd9procvalidity == mapper.validate_procedures(icd9codes, 'icd9')
-    assert expected_level_3 == mapper.map(icd9codes, 'icd9tolevel3')
-    assert expected_cci == mapper.map(icd9codes, 'icd9tocci')
 
