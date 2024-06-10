03a907e9db22eaf74b79c8bffeecd1ddaae693ba
Gregor Ehrensperger <gregor.ehrensperger@datalabhell.at>
2024-06-10 10:50:37 +0200

    feat: provide icd10 to blocks mapper

--- icdmappings/mappers/__init__.py
+++ icdmappings/mappers/__init__.py
@@ -4,9 +4,10 @@ from .icd9_to_chapters import ICD9toChapters
 from .icd9_to_cci import ICD9toCCI
 from .icd9_to_icd10 import ICD9toICD10
 from .icd10_to_icd9 import ICD10toICD9
+from .icd10_to_blocks import ICD10toBlocks
 from .icd10_to_chapters import ICD10toChapters
 from .icd10_to_ccsr import ICD10toCCSR
 from .icd10_to_ccir import ICD10toCCIR
 
 
-__all__ = ["ICD9toCCS", "ICD9toChapters", "ICD9toCCI", "ICD9toICD10",  "ICD10toICD9", "ICD10toChapters", "ICD10toCCSR", "ICD10toCCIR"]
+__all__ = ["ICD9toCCS", "ICD9toChapters", "ICD9toCCI", "ICD9toICD10",  "ICD10toICD9", "ICD10toBlocks", "ICD10toChapters", "ICD10toCCSR", "ICD10toCCIR"]
